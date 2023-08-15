f592d339e8892f6738bfed259e8108bd559da615
Callum Laing <callum.laing91@hotmail.co.uk>
2023-08-15 23:36:54 +0100

    amended initial content

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import "./style.css";
-
 document.querySelector(
   "#app"
-).innerHTML = `<div><h1>This is a placeholder for my To-do app</h1></div>`;
+).innerHTML = `<div><h1>This is a placeholder for my To-do app</h1><h2>Once I create a plan of action, this text will change!</h2></div>`;
