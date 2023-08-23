97588ccb7532a453a3ea51fbdec43c5b49777732
Callum Laing <callum.laing91@hotmail.co.uk>
2023-08-23 21:09:05 +0100

    commented out task.js until fixed

--- src/task.js
+++ src/task.js
@@ -1,20 +1,20 @@
-function taskCreation() {
-  const taskPopup = document.querySelector("#mainContent");
-  taskPopup.innerHTML = `<div id="taskForm" class="hidden">
-        <form id="taskFormFields">
-            <label for="title">Title:</label>
-            <input type="text" id="title" name="title" required><br>
+// function taskCreation() {
+//   const taskPopup = document.querySelector("#mainContent");
+//   taskPopup.innerHTML = `<div id="taskForm" class="hidden">
+//         <form id="taskFormFields">
+//             <label for="title">Title:</label>
+//             <input type="text" id="title" name="title" required><br>
 
-            <label for="description">Description:</label>
-            <textarea id="description" name="description" required></textarea><br>
+//             <label for="description">Description:</label>
+//             <textarea id="description" name="description" required></textarea><br>
 
-            <label for="dueDate">Due Date:</label>
-            <input type="date" id="dueDate" name="dueDate" required><br>
+//             <label for="dueDate">Due Date:</label>
+//             <input type="date" id="dueDate" name="dueDate" required><br>
 
-            <button type="submit">Submit</button>
-        </form>
-    </div>`;
-  return taskPopup;
-}
+//             <button type="submit">Submit</button>
+//         </form>
+//     </div>`;
+//   return taskPopup;
+// }
 
-export default taskCreation;
+// export default taskCreation;
